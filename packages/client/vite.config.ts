d988621a58b8f2789218b466e57a72c1ad899f68
collinrijock <collinrijock@gmail.com>
2025-05-18 00:50:40 -0400

    chore: build

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig(({ mode }) => ({
   envDir: "../../",
   build: {
+    minify: false,
     rollupOptions: {
       output: {
         manualChunks: {
