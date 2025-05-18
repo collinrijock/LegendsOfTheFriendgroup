48c758b7655157a2744038f8a03a1f90a09ca586
collinrijock <collinrijock@gmail.com>
2025-05-17 20:59:03 -0400

    chore: build

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,41 +1,40 @@
-// vite.config.js
+// vite.config.ts
 import { defineConfig } from "vite";
-import { terser } from "rollup-plugin-terser";
 
-export default ({ mode }) => {
-  const isLocalhost = process.env.NODE_ENV === "development";
-
-  return defineConfig({
-    envDir: "../../",
-    build: {
-      minify: "terser",
-      terserOptions: {
-        mangle: false,
+export default defineConfig(({ mode }) => ({
+  envDir: "../../",
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      mangle: {
+        // preserve your class & function names
         keep_classnames: true,
         keep_fnames: true,
       },
+      // you can also disable compression if you want
+      compress: false,
     },
+  },
 
-    server: {
-      port: 3000,
-      proxy: {
-        "/.proxy/assets": {
-          target: "http://localhost:3000/assets",
-          changeOrigin: true,
-          ws: true,
-          rewrite: (path) => path.replace(/^\/.proxy\/assets/, ""),
-        },
-        "/.proxy/api": {
-          target: "http://localhost:4001",
-          changeOrigin: true,
-          secure: false,
-          ws: true,
-          rewrite: (path) => path.replace(/^\/.proxy\/api/, ""),
-        },
+  server: {
+    port: 3000,
+    proxy: {
+      "/.proxy/assets": {
+        target: "http://localhost:3000/assets",
+        changeOrigin: true,
+        ws: true,
+        rewrite: (p) => p.replace(/^\/.proxy\/assets/, ""),
       },
-      hmr: {
-        clientPort: isLocalhost ? 3000 : 443,
+      "/.proxy/api": {
+        target: "http://localhost:4001",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (p) => p.replace(/^\/.proxy\/api/, ""),
       },
     },
-  });
-};
+    hmr: {
+      clientPort: mode === "development" ? 3000 : 443,
+    },
+  },
+}));
